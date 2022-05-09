1070b4e26032ade22135b77c904376a4c4433f34
LEEHM97 <gmlkd2006@gmail.com>
2022-05-09 21:56:57 +0900

    feat: Add return winner
    return winners name when player got total 100over scores

--- pig_the_dice.py
+++ pig_the_dice.py
@@ -5,39 +5,55 @@ import random as rand
 num = int(input('player num: '))
 player = {str(p):0 for p in range(1, num+1)}
 player['com'] = 0
+winner = ''
 
-for i in player.keys():
-    score = 0
+while(True):
 
-    while(True):
-        print(f'{i} players turn.')
-        
-        if i == 'com':
-            for roll in range(rand.randrange(0,10)):
+    for i in player.keys():
+        score = 0
+
+        while(True):
+            print(f'{i} players turn.')
+            if i == 'com':
+                for roll in range(rand.randrange(0,10)):
+                    n = rand.randrange(1,7)
+                    if n==1:
+                        player[i] = 0
+                        break
+                    else:
+                        score += n
+                        player[i] += score
+                        if player[i] >= 100:
+                            winner = i
+                            break
+                print(f'yout turn is over. score {score} saved.')
+                break
+
+
+            roll_or_stop = input('roll or stop >> ')
+            if roll_or_stop == 'roll':
                 n = rand.randrange(1,7)
+                print(f'your number is {n}')
                 if n==1:
+                    print('you got number 1, so your score return to 0 and turn over')
                     player[i] = 0
                     break
-                else:
+                else: 
                     score += n
-            player[i] += score
-            print(f'yout turn is over. score {score} saved.')
-            break
-
-
-        roll_or_stop = input('roll or stop >> ')
-        if roll_or_stop == 'roll':
-            n = rand.randrange(1,7)
-            print(f'your number is {n}')
-            if n==1:
-                print('you got number 1, so your score return to 0 and turn over')
-                player[i] = 0
-                break
+                    print(f'total score in this turn: {score}')
+                    player[i] += score
+                    if player[i] >= 100:
+                        winner = i
+                        break
             else:
-                score += n
-                print(f'total score in this turn: {score}')
-        else:
-            player[i] += score
-            print(f'your turn is over. score {score} saved.')
-            break
-print(player)
+                player[i] += score
+                print(f'your turn is over. score {score} saved.')
+                break
+
+    if winner in player:
+        print(player)
+        print(f'winnder is {winner}!')
+        break
+
+    print('--now score--')
+    print(player)
