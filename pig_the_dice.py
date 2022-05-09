43692601920eba9e44687f53be294b9fb1112dfb
LEEHM97 <gmlkd2006@gmail.com>
2022-05-09 20:56:55 +0900

    feat: Add roll or stop the dice program
    if player select 'roll' then, dice return 1~6 number
    if player select 'stop' then, current players turn is over and next
    player roll the dice

--- pig_the_dice.py
+++ pig_the_dice.py
@@ -5,4 +5,13 @@ import random as rand
 num = int(input('플레이어 수: '))
 player = {str(p):0 for p in range(1, num+1)}
 player['com'] = 0
-print(player)
+
+for i in player.keys():
+    while(True):
+        print(f'{i}번째 player님의 차례입니다.')
+        roll_or_stop = input('roll or stop >> ')
+        if roll_or_stop == 'roll':
+            n = rand.randrange(1,7)
+            print(f'숫자 {n}이 나왔습니다.')
+        else:
+            break
