bd4b93e7a4508e1ce0c2c528a3a15182cc0e3877
LEEHM97 <gmlkd2006@gmail.com>
2022-05-19 23:40:00 +0900

    modify stop(), calc_score()

--- pig_the_dice.py
+++ pig_the_dice.py
@@ -12,75 +12,62 @@ def roll():
     print(f'your number is {n}')
     return n
 
-def stop(i, score):
-    player[i] += score   
+def stop(score, i):  
+    player[i] += score
     print(f'your turn is over. score {score} saved.')
     
-def winner(i):
-    if player[i] >= 50:
-        winner = i
-    return winner
+def winner_check(i):
+    if player[i] >= 30:
+        winner = str(i)
+        return winner
+    else:
+        return 0
     
-def com_play():
-    for roll in range(rand.randrange(0,10)):
-        n = roll()
-        if n==1:
-            player[i] = 0
-            break
-        else:
-            score += n
-            player[i] += score
-            winner = winner()
-    print(f'your turn is over. score {score} saved.')
-
+def calc_score(score, n):
+        score += n
+        print(f'total score in this turn: {score}')
+        return score    
 
 while(True):
-
+    
+    if winner in player:
+        print(player)
+        print(f'winnder is {winner}!')
+        break
+    
     for i in player.keys():
         score = 0
 
         while(True):
             print(f'{i} players turn.')
             if i == 'com':
-                for roll in range(rand.randrange(0,10)):
-                    n = rand.randrange(1,7)
+                for _ in range(rand.randrange(0,10)):
+                    n = roll()
                     if n==1:
-                        player[i] = 0
+                        print('you got number 1, so your score return to 0 and turn over')
+                        player['com'] = 0
                         break
                     else:
-                        score += n
-                        player[i] += score
-                        if player[i] >= 150:
-                            winner = i
-                            break
-                print(f'yout turn is over. score {score} saved.')
+                        score = calc_score(score, n)
+                stop(score, 'com')
+                winner = winner_check(i)
                 break
-
-
+            
             roll_or_stop = input('roll or stop >> ')
             if roll_or_stop == 'roll':
-                n = rand.randrange(1,7)
-                print(f'your number is {n}')
+                n = roll()
                 if n==1:
                     print('you got number 1, so your score return to 0 and turn over')
                     player[i] = 0
                     break
                 else: 
-                    score += n
-                    print(f'total score in this turn: {score}')
-                    player[i] += score
-                    if player[i] >= 150:
-                        winner = i
-                        break
-            else:
-                player[i] += score
-                print(f'your turn is over. score {score} saved.')
-                break
+                    score = calc_score(score, n)
 
-    if winner in player:
-        print(player)
-        print(f'winnder is {winner}!')
-        break
+            else:
+                stop(score, i)
+                winner = winner_check(i)
+                break
 
     print('--now score--')
     print(player)
+
